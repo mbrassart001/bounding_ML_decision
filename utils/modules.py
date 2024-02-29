16cfcc1dc8979460f08d7148e795e6f54de1852d
mbrassart001 <mbrassart001@enseirb-matmeca.fr>
2024-02-29 13:42:49 +0100

    max layer (v1 OR)

---
+++ utils/modules.py
@@ -0,0 +1,67 @@
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
+import torch
+import more_torch_functions as mtf
+
+from collections import OrderedDict        
+
+class Parallel(torch.nn.ModuleDict):
+    def __init__(self, modules=None):
+        super().__init__(modules=modules)
+
+    def forward(self, input):
+        d = OrderedDict()
+        for name, module in self.items():
+            d[name] = module(input)
+
+        return d
+
+    # def backward(self):
+    #     pass
+
+class MaxFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        inputs = input.unbind()
+
+        # maximum ? addition ? some sort of mean ? mix of the methods ?
+        output = mtf.maximum(inputs)
+
+        ctx.save_for_backward(input, output)
+        return output
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, output = ctx.saved_tensors
+        return MaxFunction.backward_blame_all(input, output, grad_output)
+    
+    # /!\ improve needed /!\
+    @staticmethod
+    def backward_blame_max(input, output, grad_output):
+        return torch.where(input >= output, grad_output, 0)
+    
+    @staticmethod
+    def backward_blame_all(input, output, grad_output):
+        return grad_output.repeat((input.size(0),1,1))
+
+class MaxLayer(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, inputs):
+        if not isinstance(inputs, OrderedDict):
+            raise ValueError("Use OrderedDict")
+        
+        input = torch.stack(list(inputs.values()))
+
+        return MaxFunction.apply(input)
+    
+class DisplayLayer(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        print("display", type(input), input)
+        return input
---
+++ utils/more_torch_functions.py
@@ -0,0 +1,40 @@
+import torch
+import functools
+import itertools
+
+from torch import Tensor
+from typing import Callable, Iterable
+
+def reset_model(model: torch.nn.Module) -> None:
+    for layer in model.modules():
+        if hasattr(layer, "reset_parameters"):
+            layer.reset_parameters()
+
+def _apply_reduce_(func: Callable[[Tensor, Tensor], Tensor], tensors: Iterable[Tensor]) -> Tensor:
+    if len(tensors) < 1:
+        raise ValueError
+    return functools.reduce(func, tensors)
+
+def logical_big_or(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.logical_or, tensors)
+
+def bitwise_big_or(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.bitwise_or, tensors)
+
+def logical_big_and(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.logical_and, tensors)
+
+def bitwise_big_and(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.bitwise_and, tensors)
+
+def logical_cascade_xor(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.logical_xor, tensors)
+
+def bitwise_cascade_xor(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.bitwise_xor, tensors)
+
+def maximum(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.maximum, tensors)
+
+def minimum(tensors: Iterable[Tensor]) -> Tensor:
+    return _apply_reduce_(torch.minimum, tensors)
