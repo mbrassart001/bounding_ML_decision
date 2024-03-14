92e2d1d5bc9da64f7a62526accce55193259c6c5
mbrassart001 <mbrassart001@enseirb-matmeca.fr>
2024-03-14 10:33:24 +0100

    loss_fn -> criterion

--- utils/misc.py
+++ utils/misc.py
@@ -40,12 +40,12 @@ def cov_score(y_true, y_pred):
 
     return scores
 
-def train_model(x, y, model, loss_fn, optimizer, max_epoch):
+def train_model(x, y, model, criterion, optimizer, max_epoch):
     for _ in range(max_epoch):
         model.train()
         y_pred = model(x)
         
-        loss = loss_fn(y_pred, y)
+        loss = criterion(y_pred, y)
 
         model.zero_grad()
         loss.backward()
@@ -53,7 +53,7 @@ def train_model(x, y, model, loss_fn, optimizer, max_epoch):
 
     return y_pred
 
-def cross_valid(X, Y, model, loss_fn, optimizer, skf, *hooks_data, **kw_train):
+def cross_valid(X, Y, model, criterion, optimizer, skf, *hooks_data, **kw_train):
     for train_index, test_index in skf.split(X, Y):
         x_train, x_test = X[train_index], X[test_index]
         y_train, y_test = Y[train_index], Y[test_index]
@@ -63,7 +63,7 @@ def cross_valid(X, Y, model, loss_fn, optimizer, skf, *hooks_data, **kw_train):
         for hook in hooks_data:
             hook(y_train, y_test)
 
-        y_pred = train_model(x_train, y_train, model, loss_fn, optimizer, **kw_train)
+        y_pred = train_model(x_train, y_train, model, criterion, optimizer, **kw_train)
         y_pred_train = y_pred.detach().round()
         model.eval()
         y_pred_eval = model(x_test).detach()
