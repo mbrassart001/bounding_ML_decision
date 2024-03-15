f32cca740d3c119c3ad9e3914262363cfe865ae3
mbrassart001 <mbrassart001@enseirb-matmeca.fr>
2024-03-15 09:46:59 +0100

    cross valid with batch

--- utils/misc.py
+++ utils/misc.py
@@ -2,10 +2,13 @@ import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import more_torch_functions as mtf
 from sklearn import metrics
+
+from torch.utils.data import DataLoader, TensorDataset
 
 def cm(y_true, y_pred):
     confusion_matrix = metrics.confusion_matrix(y_true, y_pred)
@@ -40,7 +43,7 @@ def cov_score(y_true, y_pred):
 
     return scores
 
-def train_model(x, y, model, criterion, optimizer, max_epoch):
+def train_model(x, y, model, criterion, optimizer, max_epoch=1):
     for _ in range(max_epoch):
         model.train()
         y_pred = model(x)
@@ -53,21 +56,68 @@ def train_model(x, y, model, criterion, optimizer, max_epoch):
 
     return y_pred
 
-def cross_valid(X, Y, model, criterion, optimizer, skf, *hooks_data, **kw_train):
-    for train_index, test_index in skf.split(X, Y):
-        x_train, x_test = X[train_index], X[test_index]
-        y_train, y_test = Y[train_index], Y[test_index]
+def one_epoch(loader, model, criterion, optimizer, concat=False):
+    y_pred = torch.Tensor([]) if concat else None
+    for inputs, labels in loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        if model.training:
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+        if concat:
+            y_pred = torch.concat((y_pred, outputs.detach()))
+
+    return y_pred
+
+def train_model_batch(train_loader, model, criterion, optimizer, max_epoch=1):
+    for epoch in range(max_epoch):
+        model.train()
+        y_pred = one_epoch(train_loader, model, criterion, optimizer, concat=epoch+1 == max_epoch)
+    return y_pred
+
+def cross_valid(X, Y, model, criterion, optimizer, skf, *hooks_data, batch_size=-1, **kw_train):
+    if batch_size < 0:
+        return _cross_valid(X, Y, model, criterion, optimizer, skf, *hooks_data, **kw_train)
+    else:
+        return _cross_valid_batch(X, Y, model, criterion, optimizer, skf, batch_size, *hooks_data, **kw_train)
+
+def _cross_valid_batch(X, Y, model, criterion, optimizer, skf, batch_size, *hooks_data, **kw_train):
+    for train_index, valid_index in skf.split(X, Y):
+        x_train, x_valid = X[train_index], X[valid_index]
+        y_train, y_valid = Y[train_index], Y[valid_index]
+
+        train_dataset = TensorDataset(x_train.unsqueeze(1), y_train)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_dataset = TensorDataset(x_valid.unsqueeze(1), y_valid) 
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+        mtf.reset_model(model)
+
+        # TODO hook data
+
+        y_pred = train_model_batch(train_loader, model, criterion, optimizer, **kw_train)
+        y_pred_train = y_pred.detach().round()
+        model.eval()
+        y_pred_eval = one_epoch(val_loader, model, criterion, optimizer, concat=True).detach()
+        yield y_pred_train, y_train, y_pred_eval, y_valid
+
+def _cross_valid(X, Y, model, criterion, optimizer, skf, *hooks_data, **kw_train):
+    for train_index, valid_index in skf.split(X, Y):
+        x_train, x_valid = X[train_index], X[valid_index]
+        y_train, y_valid = Y[train_index], Y[valid_index]
 
         mtf.reset_model(model)
         
         for hook in hooks_data:
-            hook(y_train, y_test)
+            hook(y_train, y_valid)
 
         y_pred = train_model(x_train, y_train, model, criterion, optimizer, **kw_train)
         y_pred_train = y_pred.detach().round()
         model.eval()
-        y_pred_eval = model(x_test).detach()
-        yield y_pred_train, y_train, y_pred_eval, y_test
+        y_pred_eval = model(x_valid).detach()
+        yield y_pred_train, y_train, y_pred_eval, y_valid
 
 def combine_prompts(prompts, sep):
     plen = len(prompts)//2
