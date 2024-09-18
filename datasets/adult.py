dc0f5b5c68c5a5bc83578a0b00f456d1126afc29
mbrassart001 <mbrassart001@enseirb-matmeca.fr>
2024-09-18 14:10:27 +0200

    encoding sizes for each column

--- datasets/adult.py
+++ datasets/adult.py
@@ -17,3 +17,21 @@ class AdultDataset(Dataset):
     @staticmethod
     def remove_percentile(df: pd.DataFrame, pct: float) -> pd.DataFrame:
         return NotImplemented
+    
+    @staticmethod
+    def get_encoding_sizes() -> dict[str, int]:
+        return {
+            "workclass": 3,
+            "education": 4,
+            "marital-status": 2,
+            "occupation": 3,
+            "relationship": 2,
+            "race": 2,
+            "native-country": 5,
+            # "age": ,
+            # "fnlwgt": ,
+            # "education-num": ,
+            # "capital-gain": ,
+            # "capital-loss": ,
+            # "hours-per-week": ,
+        }
